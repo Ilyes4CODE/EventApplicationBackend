0474ddca1e552cfe2efaf12aa0c89d84014cee09
Larbi Mesai Ahmed <149615724+3arboo@users.noreply.github.com>
2024-04-26 12:32:14 +0100

    Update views.py add validate password and filter exists user

--- Base/auth/views.py
+++ Base/auth/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from Base.serializer import EventSeriliazer,TicketSerializer
 from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth.password_validation import validate_password
 @api_view(['GET'])
 def AuthDocumentation(request):
     Doc = {
@@ -21,20 +22,29 @@ def register(request):
     data = request.data
     serilizer = RegisterSerializer(data=data)
     if serilizer.is_valid():
-        user = User.objects.create(
+        password = data['password']
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            return Response({'msg': e.messages[0]}, status=status.HTTP_400_BAD_REQUEST)
+            
+        if not User.objects.filter(username =data['email']).exists():
+            user = User.objects.create(
             username = data['email'],
             password = make_password(data['password'])
         )
-        Profile.objects.create(
+            Profile.objects.create(
             user = user,
             first_name = data['first_name'],
             last_name = data['last_name'],
             email = data['email'],
             phone_number = data['phone_number']
         )
-        return Response({'info':'your account created'},status=status.HTTP_201_CREATED)
-    else:
-        return Response(serilizer.errors)
+            return Response({'info':'your account created'},status=status.HTTP_201_CREATED)
+        else:
+            return Response({'info':'Username not found Please create an account'})
+    return Response(serilizer.errors)
+    
     
 
 @api_view(['GET'])
